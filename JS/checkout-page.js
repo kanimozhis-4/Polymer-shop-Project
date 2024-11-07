97aa5876133fb42540a9af7bba2a9639e56c795f
kanimozhis-4 <kanimozhi808088@gmail.com>
2024-11-07 12:16:41 +0530

    removed the comment

--- JS/checkout-page.js
+++ JS/checkout-page.js
@@ -2,15 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const checkbox = document.getElementById('different-billing');
     const billingAddressDiv = document.getElementById('diff-billing-address');
 
-    // Initially hide the different billing address section
+    
     billingAddressDiv.style.display = 'none';
 
-    // Add an event listener to the checkbox
+    
     checkbox.addEventListener('change', function () {
         if (this.checked) {
-            billingAddressDiv.style.display = 'block'; // Show the billing address
+            billingAddressDiv.style.display = 'block'; 
         } else {
-            billingAddressDiv.style.display = 'none'; // Hide the billing address
+            billingAddressDiv.style.display = 'none'; 
         }
     });
 });
